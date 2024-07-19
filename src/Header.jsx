ddbfe76e7beb5058781007fbcb602de2f4228da7
sreeramlal <124680404+sreeramlal@users.noreply.github.com>
2024-07-19 16:43:50 +0530

    Update Header.jsx

--- src/Header.jsx
+++ src/Header.jsx
@@ -10,7 +10,7 @@ export default function Header() {
          
         <span className="font-bold text-xl">Workspace Reserva</span>
       </Link>
-      <div className="flex gap-2 border border-gray-300 rounded-full py-2 px-4 shadow-md shadow-gray-300">
+{/*       <div className="flex gap-2 border border-gray-300 rounded-full py-2 px-4 shadow-md shadow-gray-300">
         <div>where</div>
         <div className="border-l border-gray-300"></div>
         <div>Date</div>
@@ -21,7 +21,7 @@ export default function Header() {
             <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
           </svg>
         </button>
-      </div>
+      </div> */}
       <Link to={user?'/account':'/login'} className="flex items-center gap-2 border border-gray-300 rounded-full py-2 px-4 ">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
           <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />

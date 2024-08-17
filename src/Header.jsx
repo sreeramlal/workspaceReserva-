5dfa1964515237301a62db2bfc52c18a50e2ab7e
sreeramlal <124680404+sreeramlal@users.noreply.github.com>
2024-08-18 01:20:05 +0530

    Update Header.jsx

--- src/Header.jsx
+++ src/Header.jsx
@@ -8,7 +8,7 @@ export default function Header() {
     <header className="flex justify-between">
       <Link to={'/'} className="flex items-center gap-1">
          
-        <span className="font-bold text-xl">Workspace Reserva</span>
+        <span className="font-bold text-xl">Wow Space</span>
       </Link>
 {/*       <div className="flex gap-2 border border-gray-300 rounded-full py-2 px-4 shadow-md shadow-gray-300">
         <div>where</div>
